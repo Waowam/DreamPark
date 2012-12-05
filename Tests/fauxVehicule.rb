6a1d691ad793af750981522954b9cc5d4cbebbb0
Schin <david.eglem@gmail.com>
2012-12-05 16:43:26 +0100

    Test de la classe place OK mais fauxVehicule non implementé...

--- Tests/fauxVehicule.rb
+++ Tests/fauxVehicule.rb
@@ -1,12 +1,12 @@
 
+require '../Sources/vehicule.rb'
+
 #Classe fournissant l'instance d'un vehicule 
 class FauxVehicule < Vehicule
 
-	def initialize
-		place = []
-		for i in 0..20 do 
-			place.push(Place.new(i,rand(150..250),rand(350..450)))
-		end
-		super("DreamParkTest",place)
+	def initialize(imm="test",h=100,l=200)
+		@immatriculation=imm
+		@hauteur=h
+		@longueur=l
 	end
 end
