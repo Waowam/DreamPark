9327bcf272be3670819225daefd6d0e241ec2e50
Schin <david.eglem@gmail.com>
2012-12-05 13:35:15 +0100

    Test de la classe Place

---
+++ Tests/testPlace.rb
@@ -0,0 +1,22 @@
+#Classe TestPlace
+#
+#Classe permettant de tester les méthodes de la classe Place.
+class TestPlace
+	
+	def setup
+		@placeOK = Place.new(1,0,125,200)
+		@placeTailleNulle = Place.new(1,0,0,0)
+		@placeTailleNeg = Place.new(1,0,-10,-20)
+		@placeOccupe = Place.new(1,0,100,100)
+		@vehicule = FauxVehicule()
+	end
+	
+	def teardown
+	
+	end
+	
+	def test_occupe?
+	
+	end
+	
+end
