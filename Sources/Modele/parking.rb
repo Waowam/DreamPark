b1e81b011a74254db598ba0fdb4e75be4b28cb49
Pinto Jérémy <jeremy.pinto82@gmail.com>
2012-12-05 11:37:46 +0100

    Commit indicatif, ajout de documentation

--- Sources/Modele/parking.rb
+++ Sources/Modele/parking.rb
@@ -6,6 +6,11 @@ Eglem - Pinto
 parking.rb
 =end
 
+#Classe Parking
+#
+# Cette classe représente l'objet principal de la gestion
+# du parking. Elle gère l'entrée/sortie de véhicules
+# ainsi que la gestion des clients et des abonnées
 class Parking
 
 	attr_writer :place,:listAbonnes,:listVehicules,:nom
@@ -18,19 +23,41 @@ class Parking
 		@listClient = []
 	end
 
-	def add_vehicule(v)
-		@place.push(v)
+	#Ajoute un véhicule dans la liste correspondante.
+	#is_abonne? => listAbonne
+	#not is_abonne? => listClient
+	def add_vehicule(p,v)
+		p.vehicule = v
+		@listAbonnes.push(p) if v.is_abonne? else @listClient.push(p)
 	end
 
-	def remove_vehicule(v)
-		@place.delete(v)
+	#Remove le vehicule des listes et libère la place
+	def remove_vehicule(p,v)
+		@listAbonnes.delete(p) if v.is_abonne? else @listClient.delete(p)
+		p.vehicule = nil
 	end
 
+	#Retourne le nombre de place libre
+	def get_nb_place_libre
+		som = @listAbonnes.length+@listClient.length
+		return @place.length - som
+	end
+
+	#Retourne le nombre de place totale
 	def get_nb_place
 		return @place.length
 	end
 
-	def assigner_place
+	#Choisis une place correspondante au vehicule donné
+	def assigner_place(v)
+		@place.each do |p|
+			#blablablabla algo de recherche
+			#selon la hauteur et la longueur
+			# problematique : prendre la premier place libre avec h&l plus grand ? 
+			# 					ou
+			# 				  prendre la premiere place libre avec h&l plus grand mais de juste assez
+			# 					pour garder les plus grandes places.
+		end
 	end
 
 end
--- Sources/Modele/place.rb
+++ Sources/Modele/place.rb
@@ -6,12 +6,20 @@ Eglem - Pinto
 place.rb
 =end
 
+
+#Classe Place
+#
+#Classe représentant une place dans le parking.
+#Elle est caractérisée par son numero, son niveau
+#sa hauteur, sa longueur et son vehicule.
 class Place
 
-	attr_accessor :num,:hauteur,:longueur
+	attr_accessor :num,:niveau,:hauteur,:longueur
 
-	def initialize(num,hauteur,longueur)
+	def initialize(num,niveau,hauteur,longueur)
 		@num=num
+		@niveau=niveau
+		@vehicule=nil
 		@hauteur=hauteur
 		@longueur=longueur
 	end
