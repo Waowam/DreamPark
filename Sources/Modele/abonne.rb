9682d8cbbfe2cdf74402d1c73cdccf377a301ae7
Schin <david.eglem@gmail.com>
2012-12-11 14:27:57 +0100

    abonne.rb added

---
+++ Sources/Modele/abonne.rb
@@ -0,0 +1,25 @@
+class Abonne
+
+	attr_writer :nom,:prenom,:adresse,:tel,:hasPack
+	attr_reader :id,:nom,:prenom,:adresse,:tel
+
+	def initialize(id,nom,prenom,adresse,tel)
+		self.id=id
+		self.nom=nom
+		self.prenom=prenom
+		self.adresse=adresse
+		self.tel=tel
+		@hasPack=false
+	end
+	
+	def id=(i)
+		raise ArgumentError.new("Error : id must be positive.") if i <= 0
+		@id= i
+	end
+	
+	def has_pack?
+		self.hasPack
+	end
+			
+end
+
