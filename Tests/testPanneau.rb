28141c9b922ff6e58fbec26b5fde31a8e2706a7d
Schin <david.eglem@gmail.com>
2012-12-20 15:55:33 +0100

    Je sais plus quoi

--- Tests/testPanneau.rb
+++ Tests/testPanneau.rb
@@ -9,27 +9,55 @@ testPanneau.rb
 require "test/unit"
 require "../Sources/Modele/panneau.rb"
 
+#Test de la classe panneau
 class Test_panneau < Test::Unit::TestCase
 
+	#Test de 2 panneaux :
+	#	panNoPlace : Nombre de place max = 0
+	# 	pan : Nombre de place max = 3
 	def setup
-		@pan = Panneau.new
+		@panNoPlace = Panneau.new("testPan")
+		@pan = Panneau.new("testPan3Places",3)
 	end
 
+	#Test d'instantiacion des panneaux
 	def test_new
+		assert_instance_of(Panneau,@panNoPlace,"Erreur classe d'instance panneau")
+		assert_equal(0,@panNoPlace.placeUsed,"Erreur texte du panneau")
+		assert_equal(0,@panNoPlace.placeTot,"Erreur texte du panneau")
+		
 		assert_instance_of(Panneau,@pan,"Erreur classe d'instance panneau")
-		assert_equal(0,@pan.place,"Erreur texte du panneau")
+		assert_equal(0,@pan.placeUsed,"Erreur texte du panneau")
+		assert_equal(3,@pan.placeTot,"Erreur texte du panneau")
 	end
 
+	#Test d'incrémentation du nombre de places utilisées
 	def test_incrementer
 		@pan.incrementer
-		assert_equal(1,@pan.place)
-		@pan.decrementer
-		assert_equal(0,@pan.place)
+		assert_equal(0,@panNoPlace.placeUsed)
+
+		@pan.incrementer
+		assert_equal(1,@pan.placeUsed)
+		@pan.incrementer
+		assert_equal(2,@pan.placeUsed)
+		@pan.incrementer
+		assert_equal(3,@pan.placeUsed)
+		@pan.incrementer
+		assert_equal(3,@pan.placeUsed)
 	end
 
+	#Test de décrémentation du nombre de places utilisées
 	def test_decrementer
 		@pan.decrementer
-		assert_equal(0,@pan.place)
+		assert_equal(0,@panNoPlace.placeUsed)
+		
+		@pan.placeUsed = 2
+		@pan.decrementer
+		assert_equal(1,@pan.placeUsed)
+		@pan.decrementer
+		assert_equal(0,@pan.placeUsed)
+		@pan.decrementer
+		assert_equal(0,@pan.placeUsed)
 	end
 end
 
--- Tests/testPlace.rb
+++ Tests/testPlace.rb
@@ -5,6 +5,9 @@ require "../Sources/Modele/place.rb"
 #Classe permettant de tester les méthodes de la classe Place.
 class TestPlace < Test::Unit::TestCase
 	NUM, NIV, HAUT, LONG = 1, 0, 125, 200
+	
+	#Place de test :
+	#	placeOK : place devant être valide
 	def setup
 		@placeOK = Place.new(NUM, NIV, HAUT, LONG)
 	end
@@ -14,15 +17,20 @@ class TestPlace < Test::Unit::TestCase
 	end
 	
 	#Test des attributs apres l'instanciation d'une place
-	#placeOK : place avec des attributs correct et pas de vehicule
-	#places incorrectes (taille nulle ou négative) : lève ArgumentError
+	#	placeOK : place avec des attributs correct et pas de vehicule
+	#	places incorrectes (taille nulle ou négative) : lève ArgumentError
 	def test_new
 		assert_instance_of(Place, @placeOK, "Erreur creation placeOK")
 		assert_equal(nil, @placeOK.vehicule, "Erreur : creation de placeOK non vide")
 		
 		assert_raise(ArgumentError) {Place.new(1,0,0,0)}
-		
+		assert_raise(ArgumentError) {Place.new(1,0,10,20)}
 		assert_raise(ArgumentError) {Place.new(1,0,-10,-20)}
+	end
+	
+	#Test de l'attribut vehicule de la place
+	def test_libre
+		
 	end
 	
 end
