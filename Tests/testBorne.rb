8f595982c17d756c0294c79726d16f01b35fc0fe
Schin <david.eglem@gmail.com>
2013-01-05 20:36:06 +0100

    Tests has advanced one step. Not tested yet though... Requires files missing and comments not added.

--- Tests/testBorne.rb
+++ Tests/testBorne.rb
@@ -8,6 +8,7 @@ testBorne.rb
 
 require "test/unit"
 require "../Sources/Modele/borne.rb"
+require "./fauxParking.rb"
 
 
 #Class Test_borne
@@ -16,10 +17,35 @@ require "../Sources/Modele/borne.rb"
 class Test_borne < Test::Unit::TestCase
 
 	def setup
-		@borne = Borne.new
+		@parkFake = FauxParking.new
+		@abonneFake = FauxAbonne.new("nomTest","prenomTest","1 rue du test","0506070809")
+		@vehiculeFake = FauxVehicule.new("immTest",100,100)
+		@placeFake = FaussePlace.new(1,0,100,100)
+		@borne = Borne.new(parkFake.acces[0],"testBorne")
 	end
 
 	def test_new
-		assert_instance_of(Borne,@borne)
+		assert_instance_of(Borne,@borne,"Erreur lors de l'instanciation d'une borne")
+		assert_equals([],@borne.listTickets,"Erreur : tickets d'une nouvelle borne pas []")
+		assert_equals(false,@borne.autorisation,"Erreur : autorisation d'une nouvelle borne pas false")
 	end	
+	
+	#Test de la methode abonner
+	def test_abonner
+		@borne.abonner(@vehiculeFake,@abonneFake)
+		assert_not_equals(nil,@vehiculeFake.abonne,"Erreur : methode abonner ne lie pas le client au vehicule")
+	end
+	
+	#Test de la methode upgrade
+	def test_upgrade
+		@borne.upgrade(@abonneFake)
+		assert_equals(true,@abonneFake.hasPack,"Erreur : methode upgrade ne donne pas le pack au client")
+	end
+
+	#Test de la methode editerTicket
+	def test_editerTicket(place,v)
+		@borne.editerTicket(@placeFake,@vehiculeFake)
+		assert_equals(1,@borne.listTickets.length,"Erreur : methode editerTicket ne modifie pas le tableau des tickets")
+	end
+	
 end
--- Tests/testVehicule.rb
+++ Tests/testVehicule.rb
@@ -8,6 +8,7 @@ class TestVehicule < Test::Unit::TestCase
 	IMMAT, HAUT, LONG = "IMM_OK", 125, 200
 	
 	def setup
+		@abonneFake = FauxAbonne.new("nomTest","prenomTest","1 rue du test","0506070809")
 		@vehiculeOK = Vehicule.new(IMMAT, HAUT, LONG )
 	end
 	
@@ -21,6 +22,10 @@ class TestVehicule < Test::Unit::TestCase
 	def test_new
 		assert_instance_of(Vehicule, @vehiculeOK, "Erreur creation vehiculeOK")
 		assert_equal(nil, @vehiculeOK.abonne, "Erreur : creation de vehiculeOK -> abonne non nul")
+		assert_equal([], @vehiculeOK.livraisons, "Erreur : creation de vehiculeOK -> livraisons non []")
+		assert_equal(0, @vehiculeOK.nbreEntretien, "Erreur : creation de vehiculeOK -> nbreEntretien non nul")
+		assert_equal(0, @vehiculeOK.nbreMaintenance, "Erreur : creation de vehiculeOK -> nbreMaintenance non nul")
+		assert_equal(0, @vehiculeOK.nbreVisites, "Erreur : creation de vehiculeOK -> nbreVisites non nul")
 		
 		assert_raise(ArgumentError) {Vehicule.new("badNul",0,0)}
 		assert_raise(ArgumentError) {Vehicule.new("badNeg",-10,-20)}
@@ -38,4 +43,10 @@ class TestVehicule < Test::Unit::TestCase
 		assert_equal(0, @vehiculeOK.nbreVisites, "Erreur : visites decrementes au dela de zero")
 	end
 	
+	#Test de la méthode is_abonne?
+	def test_is_abonne?
+		assert_equal(nil, @vehiculeOK.is_abonne?, "Erreur : new vehicule -> is_abonne? non nul")
+		@vehiculeOK.abonne=abonneFake
+		assert(@vehiculeOK.is_abonne?, "Erreur : is_abonne? pas true alors qu'attribut abonne existe")
+	end
 end
